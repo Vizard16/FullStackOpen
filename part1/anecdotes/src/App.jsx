c35caa57994c4b632df7935a34f659e985dc73e1
Igmhar SC <130608853+Vizard16@users.noreply.github.com>
2024-03-12 11:12:12 -0600

    Exercises 1.12-1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,35 +1,93 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Title = (props) => {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{props.text}</h1>
+    </div>
   )
 }
 
-export default App
+const Anecdotes = (props) => {
+
+  return (
+    <div>
+      <div>{props.anecdotes}</div>
+      <div>has {props.votes} votes</div>
+    </div>
+  )
+
+}
+
+const Button = (props) => {
+  return (
+
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+
+}
+
+const MostVotes = (props) => {
+  return (
+    <div>
+      <div>{props.anecdotes}</div>
+      <div>has {props.max} votes</div>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0);
+
+  const [voted, setVoted] = useState(Array(anecdotes.length).fill(0));
+
+  const handleNextAnecdote = () => {
+    // Generate a random index for the next anecdote
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+
+    // Update the selected state with the new index
+    setSelected(randomIndex);
+  };
+
+  const handleVote = () => {
+    const newVotes = [...voted];
+    newVotes [selected] += 1;
+    setVoted(newVotes);
+  }
+  const max = Math.max(...voted)
+  const index = voted.indexOf(max)
+ 
+  return (
+
+    <div>
+      <Title text= 'Anecdote of the day'/>
+
+      <Anecdotes anecdotes={anecdotes[selected]} votes={voted[selected]}/>
+
+      <div>
+        <Button handleClick={handleVote} text="vote" />
+        <Button handleClick={handleNextAnecdote} text="next anecdote" />
+      </div>
+
+      <Title text = 'Anecdote With Most Votes' />
+
+      <MostVotes anecdotes={anecdotes[index]} max = {max} />
+      
+      
+    </div>
+  );
+};
+
+export default App;
